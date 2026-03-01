a93860ca17be170ad66f7c4fd8e6e4cc7e99292e
Kike <enriquepadilla1999@gmail.com>
2026-03-01 21:40:25 +0100

    Electron básico para pruebas

---
+++ kai-electron/preload.js
@@ -0,0 +1,11 @@
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("kai", {
+  uuid: () =>
+    (crypto?.randomUUID?.() ||
+      "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+        const r = (Math.random() * 16) | 0;
+        const v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }))
+});
